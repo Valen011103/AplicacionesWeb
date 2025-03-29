e1125e6698df2dc98498f3a20dd175cf2b5740d0
Valentina Mendez <msvalen03@gmail.com>
2025-03-29 17:37:07 -0600

    Agregar funcionalidad de la calculadora: mostrar números en pantalla y limpiar con botones

---
+++ Examen01/index.js
@@ -0,0 +1,24 @@
+const pantalla = document.getElementById('pantalla');
+const botones = document.querySelectorAll('.botonBasico');
+const botonClear = document.getElementById('boton-clear');
+const botonClearIcono = document.getElementById('boton-clear-icono');
+
+// Mostrar números en pantalla
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        if (pantalla.textContent === '0') {
+            pantalla.textContent = boton.textContent;
+        } else {
+            pantalla.textContent += boton.textContent;
+        }
+    });
+});
+
+// Limpiar pantalla con el botón "C"
+botonClear.addEventListener('click', () => {
+    pantalla.textContent = '0';
+});
+// Limpiar pantalla con el botón "icono"
+botonClearIcono.addEventListener('click', () => {
+    pantalla.textContent = '0';
+});
