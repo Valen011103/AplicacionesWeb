7cd06e32c222e1a209c9e87fc439c7579e7ea90d
Valentina Mendez <msvalen03@gmail.com>
2025-05-18 20:36:43 -0600

    guardar

--- Laboratorio2/lab02/src/App.tsx
+++ Laboratorio2/lab02/src/App.tsx
@@ -3,21 +3,59 @@ import './App.css';
 import { Options } from './Componentes/Options';
 import { Card }      from './Componentes/Card';
 import { Clickbutton } from './Componentes/Clickbutton';
+import styled from 'styled-components';
 
 
 
-const App: FC = () => (
-  <div className="container">
-    <div className="box">
-      <Options/>
-    </div>
-    <div className="box">
+// const App: FC = () => (
+//   <div className="container">
+//     <div className="box">
+//       <Options/>
+//     </div>
+//     <div className="box">
+//       <Card />
+//     </div>
+//     <div className="box">
+//       <Clickbutton />
+//     </div>
+//   </div>
+// );
+
+// export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  border-radius: 20px;
+  padding: 20px;
+  max-width: 900px;
+  margin: 40px auto;
+`;
+
+const Box = styled.div`
+  background-color: #f5b800;
+  border-radius: 15px;
+  flex: 1;
+  margin: 0 10px;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const App: React.FC = () => (
+  <Container>
+    <Box>
+      <Options />
+    </Box>
+    <Box>
       <Card />
-    </div>
-    <div className="box">
+    </Box>
+    <Box>
       <Clickbutton />
-    </div>
-  </div>
+    </Box>
+  </Container>
 );
 
 export default App;
